d054a0ec12e7d7a411d912389a82f9a669962831
Daniel Wagner <wagner.daniel87@gmail.com>
2019-06-02 12:05:05 +0200

    fix naming issues

--- src/main/java/rocks/danielw/SpringBootUserRegistrationBlueprintApplication.java
+++ src/main/java/rocks/danielw/SpringBootUserRegistrationBlueprintApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootAuthenticationBlueprintApplication {
+public class SpringBootUserRegistrationBlueprintApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringBootAuthenticationBlueprintApplication.class, args);
+    SpringApplication.run(SpringBootUserRegistrationBlueprintApplication.class, args);
   }
 	
 }
--- src/test/java/rocks/danielw/SpringBootUserRegistrationBlueprintApplicationTests.java
+++ src/test/java/rocks/danielw/SpringBootUserRegistrationBlueprintApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringBootAuthenticationBlueprintApplicationTests {
+public class SpringBootUserRegistrationBlueprintApplicationTests {
 
 	@Test
 	public void contextLoads() {
